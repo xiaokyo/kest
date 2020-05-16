aba6a8693cbdb01302db8b88233ba1623159b050
xiaokyo <904656778@qq.com>
2020-05-16 13:25:59 +0800

    done framework and start cli

--- src/__tests__/Greeter.test.ts
+++ src/__tests__/Greeter.test.ts
@@ -1,5 +1,3 @@
-import { Greeter } from "../index";
-
 test("Greeter test", () => {
-  expect(Greeter("Carl")).toBe("Hello Carl");
+  expect("1").toBe("1");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+#!/usr/bin/env node
+import { program } from "commander";
+import { bold, keyword } from "chalk";
+import { version } from "../package.json";
+
+const log = console.log;
+const error = (val: any) => log(bold.red("[ERROR]:"), val);
+const success = (val: any) => log(bold.green("[DONE]:"), val);
+const warning = (val: any) => log(keyword("orange")("[WARNING]:"), val);
+
+program
+  .version(version)
+  .command("dev [env]")
+  .description("development")
+  .action((env) => {
+    error(env);
+    success(env);
+    warning(env);
+  });
+
+program.parse(process.argv);
